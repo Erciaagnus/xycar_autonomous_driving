51df4658f49858cefbb9f6a22bfe44c4693f318e
henricus <henricus0973@gmail.com>
2024-03-27 00:46:07 +0900

    Added PID Controller. Added Process of the PID control

--- src/LaneKeepingSystem/PIDController.cpp
+++ src/LaneKeepingSystem/PIDController.cpp
@@ -1,6 +1,23 @@
 #include "LaneKeepingSystem/PIDController.hpp"
 namespace Xycar {
 
+template<typename PREC>
+PIDController<PREC>::PIDController(PREC Kp_, PREC Ki_, PREC K_d)
+    :Kp(Kp_), Ki(Ki_), Kd(Kd_), prev_error(0.0), integral(0.0), derivative(0.0){}
+
+template<typename PREC>
+PREC PIDController<PREC>::getControlOutput(PREC errorFromMid){
+    errorFromMid;
+    integral +=errorFromMid;
+    derivative=errorFromMid - prev_error;
+    prev_error=errorFromMid;
+    if(abs(errorFromMid) > 70)
+        return 0.52*errorFromMid + Ki*integral + Kd*derivative;
+
+    return Kp*errorFromMid +Ki*integral +Kd*derivative;
+
+}
+
 
 
 template class PIDController<float>;
